5476f42915e48a16aec24884b3d001761349c4c0
weileanjs <24689003@qq.com>
2017-01-07 21:58:47 +0800

    add  codes compare

--- stock_basics/stock_plot.py
+++ stock_basics/stock_plot.py
@@ -22,14 +22,18 @@ d = {"reportdate":"20160930","basiceps":'基本每股收益',"epsdiluted":'每�
 
 
 class Stock_show(object):
-    def __init__(self,code,cs='pe',q='all',sty=2010,cpl = None):
-        self.code = code
-        self.cs = cs
-        self.q = q
-        self.sty = sty
-        self.cpl =cpl
-    def to_pandas(self):
-        info = db[self.code].find({},{'_id':0,'name':0,'industry':0,'area':0,'timeToMarket':0})
+    def __init__(self,code,cs='pe',q='all',sty=2010,cpl = None,**code_kw):
+        self.code = code            #stock_code
+        self.cs = cs                 # default stock indicator
+        self.q = q                   # period   all = 年报  one = 单季报   mid = 中报+年报
+        self.sty = sty                # strat year
+        self.cpl =cpl                  # double y  indicators
+        self.codes = code_kw
+    def to_pandas(self,c =None):
+        if c == None:
+            info = db[self.code].find({},{'_id':0,'name':0,'industry':0,'area':0,'timeToMarket':0})
+        else:
+            info = db[c].find({},{'_id':0,'name':0,'industry':0,'area':0,'timeToMarket':0})
         data_ = pd.DataFrame(list(info)).T
         k_ = data_.index.tolist()
         v_ = data_.values.tolist()
@@ -37,11 +41,13 @@ class Stock_show(object):
         for i in v_:
             pds_l.append(pd.Series(eval(i[0])))
         data_DF = pd.DataFrame(pds_l,index = k_).reset_index()
-        return data_DF
+        # return data_DF
 
-
-    def get_price(self):
-        sg_priceM = dbM_p[self.code].find({},{'_id':0})
+    # def get_price(self):
+        if c == None:
+            sg_priceM = dbM_p[self.code].find({},{'_id':0})
+        else:
+            sg_priceM = dbM_p[c].find({},{'_id':0})
         priceM_ = pd.DataFrame(list(sg_priceM)).T
         idx_ = list(priceM_.index)
         Q_idx = []
@@ -58,12 +64,12 @@ class Stock_show(object):
         for y in price3M_.values:
             Q_val.append(y[0][1])
         pdP = pd.DataFrame({'price':Q_val},index = Q_idx).reset_index()
-        return pdP
-    #pdP = get_price('000625')
+        # return pdP
+
 
     #合并basic、价格
-    def merge_datas(self):
-        pdA = pd.merge(self.to_pandas(),self.get_price(),how = 'left').rename(columns={'index': 'index_t'})
+    # def merge_datas(self):
+        pdA = pd.merge( data_DF,pdP,how = 'left').rename(columns={'index': 'index_t'})
         #对年度、季度进行分列
         yeth_split = pd.DataFrame((x.split('_') for x in pdA.index_t),index=pdA.index,columns=['year','th'])
         pdA_split = pd.merge(pdA,yeth_split,right_index = True, left_index = True)
@@ -91,7 +97,7 @@ class Stock_show(object):
 
     def polshow(self):
         plt.rc('font', family='STXihei', size=9)
-        show_data = self.merge_datas().dropna(subset=[self.cs])
+        show_data = self.to_pandas().dropna(subset=[self.cs])
         a=np.array([i for i in range(1,len(show_data['index_t'])+1)])
         code_x = show_data['index_t']
         data_y1 = show_data[self.cs]
@@ -107,7 +113,7 @@ class Stock_show(object):
 
     def polshows(self):
         plt.rc('font', family='STXihei', size=9)
-        show_datas = self.merge_datas()
+        show_datas = self.to_pandas()
         a=np.array([i for i in range(1,len(show_datas['index_t'])+1)])
         code_x = show_datas['index_t']
         data_y1 = show_datas[self.cpl[0]]
@@ -133,10 +139,42 @@ class Stock_show(object):
         plt.show()
 
 
+    def stocks_comp(self):
+        if self.codes != 0:
+            y_codes = self.codes['codes'].split(',')
+            y_ccs = self.codes['ccs']              # 进行对比指标
+            pdccs_first = self.to_pandas(y_codes[0])[['index_t',y_ccs]].rename(columns={y_ccs:y_codes[0]})
+            for code in y_codes:
+                pdccs = self.to_pandas(code)[['index_t',y_ccs]].rename(columns={y_ccs:code})
+                pdccs_first = pd.merge(pdccs_first,pdccs,how = 'outer')
+            pdccs_ed = pdccs_first.fillna(0).sort('index_t')
+            pdccs_ed['mean_'] = pdccs_first.mean(axis=1)
+            plt.rc('font', family='STXihei', size=9)
+            a=np.array([i for i in range(1,len(pdccs_ed['index_t'])+1)])
+            code_x = pdccs_ed['index_t']
+            for co in y_codes:
+                code_name = db[co].find({},{'name':1})[0]['name'][0]
+                plt.plot(a, pdccs_ed[co],label = code_name, linewidth=0.9)
+            plt.plot(a, pdccs_ed['mean_'],label = '均值',linestyle='-.', linewidth=2,alpha=5)
+            plt.grid(color='#95a5a6',linestyle='--', linewidth=1,axis='y',alpha=0.4)
+            plt.xlabel('季度')
+            plt.ylabel(y_ccs)
+            if 'title' in self.codes:
+                plt.title(self.codes['title'])
+            else:
+                plt.title('%s:%s'%(y_codes,y_ccs))
+            # plt.ylim(0,50)
+            plt.xticks(a,code_x)
+            plt.legend()
+            plt.show()
+        else:
+            print('code_kw is None')
 
-#
-a = Stock_show('000581',cs='pe',q='one',sty=2007,cpl=["salegrossprofitrto","mainbusiincome"])
-a. polshow()
-a.polshows()
-a.merge_datas()
 
+code_args = {'codes':'601633,000625,600104,601238','ccs':'pe',}
+
+a = Stock_show('601633',cs='pe',q='one',sty=2005,**code_args)
+# print(a.to_pandas()[['price','pe','index_t',"basiceps"]])
+# a. polshow()
+# # a.polshows()
+a.stocks_comp()
