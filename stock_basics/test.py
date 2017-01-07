2599cd3887847ee3c0e60866eb346d1dcdf54ab9
weileanjs <24689003@qq.com>
2017-01-07 11:16:18 +0800

    ed1

--- stock_basics/test.py
+++ stock_basics/test.py
@@ -1,128 +1,158 @@
+# coding: utf-8
+#下载基础数据
+import pandas
+import os
+import pandas as pd
+import numpy as np
+import time
+import random
 import tushare as ts
 import pymongo
-import pandas as pd
-client = pymongo.MongoClient('localhost',27017)
-db = client['stock_basics']
-import json
 
 
-df_basic = pd.read_csv(r'D:\W\python\stockFdate2\stock_datas\stock_basics\stock_basics.csv',encoding='gbk')
-
-df_basic_r = df_basic.set_index('code',drop=False).loc[:,['code','industry','area','timeToMarket','name']]
-db['basic'].insert_many(df_basic_r.to_dict('records'))
-# print(df_basic_r['code'].values)
-# print( df_basic_r.loc[600000]['industry'])
-# def stoct_basic_info(code):
-#     dict_1 = {}
-#     dict_1['industry'] = df_basic_r.loc[code]['industry'],
-#     dict_1['area'] = df_basic_r.loc[code]['area'],
-#     dict_1['timeToMarket'] = str(df_basic_r.loc[code]['timeToMarket']),
-#     dict_1['name'] = df_basic_r.loc[code]['name'],
-#     db['%06d'%code].insert_one(dict_1)
-# for code in df_basic['code'].values:
-#     print('%06d'%code)
-#     stoct_basic_info(code)      #存储'industry','area','timeToMarket'
-
-'''
-# report_data_raw = ts.get_report_data(2014,3)
-# report_data_raw.to_csv('xxxx.csv')
-report_data = pd.read_csv('xxxx.csv',encoding='gbk')
-report_data_r = report_data.set_index('code',drop=True).loc[:,['eps', 'eps_yoy', 'bvps', 'roe', 'epcf', 'net_profits', 'profits_yoy', 'distrib', 'report_date']]
-report_data_r1 = report_data_r.drop_duplicates()
-
-# profit_data_raw = ts.get_profit_data(2014,3)
-# profit_data_raw.to_csv('xxxxx.csv')
-profit_data = pd.read_csv('xxxxx.csv',encoding='gbk').drop_duplicates()
-columns_p = profit_data.columns.tolist()
-# print(profit_data.head(5))
-# print(columns_p)
-# print(profit_data.index.astype(str))
-
-df3 = pd.merge(report_data.drop_duplicates(),profit_data.drop_duplicates(),on='code')
-print(len(report_data.drop_duplicates()))
-print(len(profit_data.drop_duplicates()))
-print(len(df3))
-# df3.to_csv('df333.csv')
-# print(df3.head(10))
-# profit_data_r = report_data.set_index('code',drop=True).loc[:,['eps', 'eps_yoy', 'bvps', 'roe', 'epcf', 'net_profits', 'profits_yoy', 'distrib', 'report_date']]
-# profit_data_r1 = report_data_r.drop_duplicates()
+class getbasicdata:
+    def __init__(self,startY,stopY):
+        self.startY = startY
+        self.stopY = stopY
+        client = pymongo.MongoClient('localhost',27017)
+        db = client['stock_basics']
+    def get_basic(self):
+        client = pymongo.MongoClient('localhost',27017)
+        db = client['stock_basics']
+        df_stock_basics = ts.get_stock_basics()
+        for code in df_stock_basics.index:
+            dict_1 = {}
+            dict_1['industry'] = df_stock_basics.loc[code]['industry'],
+            dict_1['area'] = df_stock_basics.loc[code]['area'],
+            dict_1['timeToMarket'] = str(df_stock_basics.loc[code]['timeToMarket']),
+            db[code].insert_one(dict_1)
 
 
+    def report_data(self):
+        for ye in range(self.startY,self.stopY):
+            for th in [1,2,3,4]:
+                df_report_data = ts.get_report_data(ye,th).drop_duplicates(),
+                df_profit_data = ts.get_profit_data(ye, th).drop_duplicates(),
+                df_operation_data = ts.get_operation_data(ye, th).drop_duplicates(),
+                df_growth_data = ts.get_growth_data(ye, th).drop_duplicates(),
+                df_debtpaying_data = ts.get_debtpaying_data(ye, th).drop_duplicates(),
+                df_cashflow_data = ts.get_cashflow_data(ye, th).drop_duplicates(),
 
+                df_combime = pd.merge(df_report_data,df_profit_data,on='code',how='outer')
+                df_combime1 = pd.merge(df_combime,df_operation_data,on='code',how='outer')
+                df_combime2 = pd.merge(df_combime1,df_growth_data,on='code',how='outer')
+                df_combime3 = pd.merge(df_combime2,df_debtpaying_data,on='code',how='outer')
+                df_combime4 = pd.merge(df_combime3,df_cashflow_data,on='code',how='outer')
 
+# def to_getstockdata(a1,a2):
+#     aaa=getbasicdata(a1,a2)
+#     aaa.report_data()
+#     aaa.profit_data()
+#     aaa.operation_data()
+#     aaa.growth_data()
+#     aaa.debtpaying_data()
+#     aaa.cashflow_data()
+#     aaa.stock_basics()
 
-# series 转 json 格式
-def series_to_json(s):
-    dict_s_j = {}
-    s_k = s.index.tolist()
-    s_v = s.values.tolist()
-    for k ,v in zip(s_k ,s_v):
-        dict_s_j[k] = str(v)
-    return str(dict_s_j)
+if __name__=="__main__":
+    p = getbasicdata
+    #start = p.get_basic('a')
+
+    #to_getstockdata(2007,2017)
 
 
 
 
-def stock_detail_data(code,th):
-    dict_1 = {}
-    dict_1[th] = str(report_data_r1.loc[code])
-    s = report_data_r1.loc[code]
-    ret = series_to_json(s)
-    db[str(code)].insert_one({th:str(ret)})
+# df3 = pd.merge(report_data.drop_duplicates(),profit_data.drop_duplicates(),on='code')
+# print(len(report_data.drop_duplicates()))
 
 
-def stock_profit_data(code,th):
-    dict_1 = {}
-    dict_1[th] = str(report_data_r1.loc[code])
-    s = report_data_r1.loc[code]
-    ret = series_to_json(s)
-    db[str(code)].insert_one({th:str(ret)})
-
-
-
-# for co in report_data_r1.index:
-#     print(co)
-#     stoct_report_data(co,'2014-3')
-#
-#
-# read_ = db['600720'].find({},{'2014-3':1,"_id":0})
-# for i in read_:
-#     print(i)
-# a1 = list(read_[2].values())[0]
-# print(a1)
-# a2 = eval(a1)
-# a3 = pd.Series(a2)
-# print(a3['roe'])
-
-
-# roe,净资产收益率(%)
-# net_profit_ratio,净利率(%)
-# gross_profit_rate,毛利率(%)
-# net_profits,净利润(万元)
-# esp,每股收益
-# business_income,营业收入(百万元)
-# bips,每股主营业务收入(元)
-# code,代码
-# name,名称
-# industry,所属行业
-# area,地区
-# pe,市盈率
-# outstanding,流通股本(亿)
-# totals,总股本(亿)
-# totalAssets,总资产(万)
-# liquidAssets,流动资产
-# fixedAssets,固定资产
-# reserved,公积金
-# reservedPerShare,每股公积金
-# esp,每股收益
-# bvps,每股净资
-# pb,市净率
-# timeToMarket,上市日期
-# undp,未分利润
-# perundp, 每股未分配
-# rev,收入同比(%)
-# profit,利润同比(%)
-# gpr,毛利率(%)
-# npr,净利润率(%)
-# holders,股东人数
-'''
+    #             if os.path.isfile("stock_datas\\report_data\\report_data%s%s.csv" %(ye,th)):
+    #                 print("report_data%s%s.csv" %(ye,th)+'  is exist')
+    #             else:
+    #                 try:
+    #                     print("report_data%s%s.csv" %(ye,th)+'  is downloading')
+    #                     time.sleep(random.random()*2)
+    #                     df_report_data = ts.get_report_data(ye,th)
+    #                     df_report_data.to_csv('stock_datas\\report_data\\report_data%s%s.csv' %(ye,th))
+    #                 except AttributeError:
+    #                     print("report_data%s%s.csv" %(ye,th)+'  and later is not Allowed')
+    #                     break
+    #
+    # def profit_data(self):
+    #     for ye in range(2007, 2016):
+    #         for th in [1, 2, 3, 4]:
+    #             if os.path.isfile("stock_datas\\profit_data\\profit_data%s%s.csv" % (ye, th)):
+    #                 print("profit_data%s%s.csv" % (ye, th) + '  is exist')
+    #             else:
+    #                 try:
+    #                     print("profit_data%s%s.csv" % (ye, th) + '  is downloading')
+    #                     time.sleep(random.random() * 2)
+    #                     df_profit_data = ts.get_profit_data(ye, th)
+    #                     df_profit_data.to_csv('stock_datas\\profit_data\\profit_data%s%s.csv' % (ye, th))
+    #                 except AttributeError:
+    #                     print("profit_data%s%s.csv" % (ye, th) + ' and later is not Allowed')
+    #                     break
+    #
+    # def operation_data(self):
+    #     for ye in range(self.startY, self.stopY):
+    #         for th in [1, 2, 3, 4]:
+    #             if os.path.isfile("stock_datas\\operation_data\\operation_data%s%s.csv" % (ye, th)):
+    #                 print("operation_data%s%s.csv" % (ye, th) + '  is exist')
+    #             else:
+    #                 try:
+    #                     print("operation_data%s%s.csv" % (ye, th) + '  is downloading')
+    #                     time.sleep(random.random() * 2)
+    #                     df_operation_data = ts.get_operation_data(ye, th)
+    #                     df_operation_data.to_csv('stock_datas\\operation_data\\operation_data%s%s.csv' % (ye, th))
+    #                 except AttributeError:
+    #                     print("operation_data%s%s.csv" % (ye, th) + '  and later is not Allowed')
+    #                     break
+    #
+    #
+    # def growth_data(self):
+    #     for ye in range(self.startY, self.stopY):
+    #         for th in [1, 2, 3, 4]:
+    #             if os.path.isfile("stock_datas\\growth_data\\growth_data%s%s.csv" % (ye, th)):
+    #                 print("growth_data%s%s.csv" % (ye, th) + '  is exist')
+    #             else:
+    #                 try:
+    #                     print("growth_data%s%s.csv" % (ye, th) + '  is downloading')
+    #                     time.sleep(random.random() * 2)
+    #                     df_growth_data = ts.get_growth_data(ye, th)
+    #                     df_growth_data.to_csv('stock_datas\\growth_data\\growth_data%s%s.csv' % (ye, th))
+    #                 except AttributeError:
+    #                     print("growth_data%s%s.csv" % (ye, th) + '  and later is not Allowed')
+    #                     break
+    #
+    #
+    # def debtpaying_data(self):
+    #     for ye in range(self.startY, self.stopY):
+    #         for th in [1, 2, 3, 4]:
+    #             if os.path.isfile("stock_datas\\debtpaying_data\\debtpaying_data%s%s.csv" % (ye, th)):
+    #                 print("debtpaying_data%s%s.csv" % (ye, th) + '  is exist')
+    #             else:
+    #                 try:
+    #                     print("debtpaying_data%s%s.csv" % (ye, th) + '  is downloading')
+    #                     time.sleep(random.random() * 2)
+    #                     df_debtpaying_data = ts.get_debtpaying_data(ye, th)
+    #                     df_debtpaying_data.to_csv('stock_datas\\debtpaying_data\\debtpaying_data%s%s.csv' % (ye, th))
+    #                 except AttributeError:
+    #                     print("debtpaying_data%s%s.csv" % (ye, th) + '  and later is not Allowed')
+    #                     break
+    # def cashflow_data(self):
+    #     for ye in range(self.startY, self.stopY):
+    #         for th in [1, 2, 3, 4]:
+    #             if os.path.isfile("stock_datas\\cashflow_data\\cashflow_data%s%s.csv" % (ye, th)):
+    #                 print("cashflow_data%s%s.csv" % (ye, th) + '  is exist')
+    #             else:
+    #                 try:
+    #                     print("cashflow_data%s%s.csv" % (ye, th) + '  is downloading')
+    #                     time.sleep(random.random() * 2)
+    #                     df_cashflow_data = ts.get_cashflow_data(ye, th)
+    #                     df_cashflow_data.to_csv('stock_datas\\cashflow_data\\cashflow_data%s%s.csv' % (ye, th))
+    #                 except AttributeError:
+    #                     print("cashflow_data%s%s.csv" % (ye, th) + '  and later is not Allowed')
+    #                     break
+    #
+    #
